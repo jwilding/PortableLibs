ed7ccfd9c0e1409727c3529695dba7ce7fc84ade
Joshua Wilding <joshua_wilding@hotmail.com>
2015-05-08 01:56:33 +0800

    UPDATED: "Phonegap-Initialize.bat".
    NOTE: Use "Phonegap\Phonegap-Initialize.bat".

--- Phonegap/! Initialize/Initialize.bat
+++
@@ -1,8 +0,0 @@
-@ECHO ON
-PUSHD ".."
-CALL npm cache clean
-CALL npm cache clean
-CALL npm install -g
-CALL "%~dp0overrides\override.bat"
-POPD
-PAUSE
--- Phonegap/overrides/override.bat
+++ Phonegap/overrides/override.bat
@@ -1,4 +1,4 @@
-@PUSHD "%~dp0..\.."
+@PUSHD "%~dp0.."
 @CALL XCOPY "%~dp0node_modules\*" ".\node_modules\" /S /Y
 @CALL NODE "%~dp0phonegap-fix.js"
 @POPD
