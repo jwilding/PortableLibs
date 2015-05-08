f1b0d398720e7f1f66bb71b551dbd986c1f8e143
Joshua Wilding <joshua_wilding@hotmail.com>
2015-05-08 10:38:37 +0800

    FIXED: "Phonegap\Phonegap-Initialize.bat".

--- Phonegap/Phonegap-Initialize.bat
+++ Phonegap/Phonegap-Initialize.bat
@@ -1,7 +1,7 @@
 @ECHO ON
 ::CALL npm cache clean
 ::CALL npm cache clean
-CALL npm install
-CALL XCOPY "%~dp0node_modules\.bin\*.cmd" ".\" /S /Y
+CALL npm install -g cordova-gen pgbuild phonegap
+::CALL XCOPY "%~dp0node_modules\.bin\*.cmd" ".\" /S /Y
 CALL "%~dp0overrides\override.bat"
 PAUSE
